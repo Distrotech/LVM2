dd392346cccffa33d98df99fcaa2af0f1f2e6965
Petr Rockai <prockai@redhat.com>
2013-05-27 13:25:27 +0200

    test: Do not fail when flavour overrides are empty.

--- test/lib/test.sh
+++ test/lib/test.sh
@@ -57,6 +57,7 @@ cd "$TESTDIR"
 echo "$TESTNAME" >TESTNAME
 
 if test -n "$LVM_TEST_FLAVOUR"; then
+    touch flavour_overrides
     env | grep ^$LVM_TEST_FLAVOUR | while read var; do
 	(echo -n "export "; echo $var | sed -e s,^${LVM_TEST_FLAVOUR}_,,) >> flavour_overrides
     done
