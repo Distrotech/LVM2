7a9c1b63daf46edca3fb87bdbae3fe73ec3b04cf
Ondrej Kozina <okozina@redhat.com>
2015-03-05 15:49:56 +0100

    lvmpolld-client: respect LVMPOLLD_SUPPORT #define

--- daemons/lvmpolld/lvmpolld-client.h
+++ daemons/lvmpolld/lvmpolld-client.h
@@ -14,14 +14,14 @@
 
 #ifndef _LVM_LVMPOLLD_CLIENT_H
 #define _LVM_LVMPOLLD_CLIENT_H
+#  ifdef LVMPOLLD_SUPPORT
 
-#include "daemon-client.h"
+#	include "daemon-client.h"
+
+#	define LVMPOLLD_SOCKET DEFAULT_RUN_DIR "/lvmpolld.socket"
 
 struct cmd_context;
 
-#define LVMPOLLD_SOCKET DEFAULT_RUN_DIR "/lvmpolld.socket"
-
-/* daemon_handle lvmpolld_connect(const char *socket);*/
 void lvmpolld_disconnect(void);
 
 int lvmpolld_poll_init(const struct cmd_context *cmd, const char *vgname,
@@ -35,5 +35,16 @@ int lvmpolld_use(void);
 void lvmpolld_set_active(int active);
 
 void lvmpolld_set_socket(const char *socket);
+
+#  else
+
+#	define lvmpolld_disconnect() do {} while (0)
+#	define lvmpolld_poll_init(cmd, vgname, lvname, uuid, lv_type, interval, abort) (0)
+#	define lvmpolld_request_info(uuid, abort, finished) (0)
+#	define lvmpolld_use() (0)
+#	define lvmpolld_set_active(active) do {} while (0)
+#	define lvmpolld_set_socket(socket) do {} while (0)
+
+#  endif /* LVMPOLLD_SUPPORT */
 
 #endif /* _LVM_LVMPOLLD_CLIENT_H */
