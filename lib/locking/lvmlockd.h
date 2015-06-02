c11521a06195098b950d4c71f140435b1f2a2b41
David Teigland <teigland@redhat.com>
2015-06-02 10:03:11 -0500

    lvmlockd: use LV ids for LV lock names

--- lib/locking/lvmlockd.h
+++ lib/locking/lvmlockd.h
@@ -128,20 +128,20 @@ int lockd_vg(struct cmd_context *cmd, const char *vg_name, const char *def_mode,
 int lockd_vg_update(struct volume_group *vg);
 
 int lockd_lv_name(struct cmd_context *cmd, struct volume_group *vg,
-		  const char *lv_name, const char *lock_args,
-		  const char *def_mode, uint32_t flags);
+		  const char *lv_name, struct id *lv_id,
+		  const char *lock_args, const char *def_mode, uint32_t flags);
 int lockd_lv(struct cmd_context *cmd, struct logical_volume *lv,
 	     const char *def_mode, uint32_t flags);
 
 /* lvcreate/lvremove use init/free */
 
 int lockd_init_lv(struct cmd_context *cmd, struct volume_group *vg,
-		  const char *lv_name, struct lvcreate_params *lp);
+		  const char *lv_name, struct id *lv_id, struct lvcreate_params *lp);
 int lockd_free_lv(struct cmd_context *cmd, struct volume_group *vg,
-		  const char *lv_name, const char *lock_args);
+		  const char *lv_name, struct id *lv_id, const char *lock_args);
 
 int lockd_init_lv_args(struct cmd_context *cmd, struct volume_group *vg,
-		       const char *lv_name, const char *lock_type, const char **lock_args);
+		       const char *lv_name, struct id *lv_id, const char *lock_type, const char **lock_args);
 
 const char *lockd_running_lock_type(struct cmd_context *cmd);
 
@@ -230,8 +230,8 @@ static inline int lockd_vg_update(struct volume_group *vg)
 }
 
 static inline int lockd_lv_name(struct cmd_context *cmd, struct volume_group *vg,
-		  const char *lv_name, const char *lock_args,
-		  const char *def_mode, uint32_t flags)
+		  const char *lv_name, struct id *lv_id,
+		  const char *lock_args, const char *def_mode, uint32_t flags)
 {
 	return 1;
 }
@@ -243,19 +243,19 @@ static inline int lockd_lv(struct cmd_context *cmd, struct logical_volume *lv,
 }
 
 static inline int lockd_init_lv(struct cmd_context *cmd, struct volume_group *vg,
-		  const char *lv_name, struct lvcreate_params *lp)
+		  const char *lv_name, struct id *lv_id, struct lvcreate_params *lp)
 {
 	return 0;
 }
 
 static inline int lockd_free_lv(struct cmd_context *cmd, struct volume_group *vg,
-		  const char *lv_name, const char *lock_args)
+		  const char *lv_name, struct id *lv_id, const char *lock_args)
 {
 	return 0;
 }
 
 static inline int lockd_init_lv_args(struct cmd_context *cmd, struct volume_group *vg,
-		       const char *lv_name, const char *lock_type, const char **lock_args)
+		       const char *lv_name, struct id *lv_id, const char *lock_type, const char **lock_args)
 {
 	return 0;
 }
