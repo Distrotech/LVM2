9ff0063f1d1a10be0c1aa75056d28e8b04d2731d
David Teigland <teigland@redhat.com>
2015-09-21 13:50:32 -0500

    lvmnotify: add initial dbus notification
    This follows the pattern of lvmlockd, which
    notifies lvmlockd whenever a VG is updated,
    providing the VG name and new seqno.
    
    Building required setting the GIO_CFLAGS and
    GIO_LIBS env var explicitly for some reason.
    
    Excluding applib from the configure.
    
    export GIO_CFLAGS='-pthread -I/usr/include/gio-unix-2.0/ -I/usr/include/glib-2.0 -I/usr/lib64/glib-2.0/include'
    (pkg-config --cflags gio-unix-2.0)
    
    export GIO_LIBS='-lgio-2.0 -lgobject-2.0 -lglib-2.0'
    (pkg-config --libs gio-unix-2.0)
    
    autoreconf
    
    ./configure --build=x86_64-redhat-linux-gnu --host=x86_64-redhat-linux-gnu
    --program-prefix= --disable-dependency-tracking --prefix=/usr
    --exec-prefix=/usr --bindir=/usr/bin --sbindir=/usr/sbin --sysconfdir=/etc
    --datadir=/usr/share --includedir=/usr/include --libdir=/usr/lib64
    --libexecdir=/usr/libexec --localstatedir=/var --sharedstatedir=/var/lib
    --mandir=/usr/share/man --infodir=/usr/share/info
    --with-default-dm-run-dir=/run --with-default-run-dir=/run/lvm
    --with-default-pid-dir=/run --with-default-locking-dir=/run/lock/lvm
    --with-usrlibdir=/usr/lib64 --enable-fsadm --with-pool=internal
    --enable-write_install --with-user= --with-group= --with-device-uid=0
    --with-device-gid=6 --with-device-mode=0660 --enable-pkgconfig
    --enable-cmdlib --with-cluster=internal --with-clvmd=corosync
    --with-udevdir=/usr/lib/udev/rules.d --enable-udev_sync
    --with-thin=internal --with-thin-check=/usr/sbin/thin_check
    --with-thin-dump=/usr/sbin/thin_dump
    --with-thin-repair=/usr/sbin/thin_repair --enable-lvmetad
    --enable-udev-systemd-background-jobs --with-cache=internal
    --enable-lvmpolld --with-clvmd=none --enable-notify-dbus

---
+++ lib/notify/lvmnotify.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2015 Red Hat, Inc.
+ *
+ * This file is part of LVM2.
+ *
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License v.2.1.
+ */
+
+#ifndef _LVMNOTIFY_H
+#define _LVMNOTIFY_H
+
+int lvmnotify_init(struct cmd_context *cmd);
+void lvmnotify_exit(void);
+
+void notify_vg_update(struct volume_group *vg);
+
+#endif
+
